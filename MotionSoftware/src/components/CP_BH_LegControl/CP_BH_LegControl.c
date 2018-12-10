0126761e5b7433cbad5a35b6f2e67ee7889bc2a4
woookey <woo.barczyk@gmail.com>
2018-12-10 23:09:18 +0000

    LegControl module waits for power up

--- MotionSoftware/src/components/CP_BH_LegControl/CP_BH_LegControl.c
+++ MotionSoftware/src/components/CP_BH_LegControl/CP_BH_LegControl.c
@@ -1,64 +1,63 @@
 #include <CP_BH_LegControl.h>
 #include <assert.h>
+#include <RF_dispatcher.h>
 #include <RF_timers.h>
+#include <systemSignals.h>
 
 typedef struct
 {
-	RFAgent* baseAgent;
-	RF_Timer* LegControlWaitForPowerUpTimeout;
+	RFAgent baseAgent;
 } LegControlModule_t;
 
 static LegControlModule_t LegControlModuleInstance;
 RFAgent * const LegControlModule = (RFAgent* const)&LegControlModuleInstance;
 
-typedef enum
-{
-	LEG_CONTROL_WAIT_FOR_POWER_UP_SIGNAL = SS_END_OF_SIGNAL_SPACE,
-} PrivateSignals;
-
-typedef enum
-{
-	LEG_CONTROL_WAIT_FOR_POWER_UP_TIMEOUT_IN_MS = 1000U,
-} PrivateDefinitions;
-
-
 static RFHandle InitialState(LegControlModule_t* const me, RFEvent *const evt);
-static RFHandle NoControlState(LegControlModule_t* const me, RFEvent *const evt);
+static RFHandle WaitForPower(LegControlModule_t* const me, RFEvent *const evt);
+static RFHandle NoMotionState(LegControlModule_t* const me, RFEvent *const evt);
 
 void CP_BH_LegControlModuleConstructor(RFAgent * const self)
 {
 	assert(self == LegControlModule);
 	LegControlModule_t *const me = (LegControlModule_t* const)self;
-	RFBaseAgentConstructor((RFAgent*)me, &InitialState);
-	RFTimerConstructor((RFAgent*) me, (RF_Timer*)&me->LegControlWaitForPowerUpTimeout, LEG_CONTROL_WAIT_FOR_POWER_UP_SIGNAL);
+	RFBaseAgentConstructor((RFAgent*)&me, &InitialState);
+	subscribeAgentToSignal((RFAgent*)&me, SS_CP_BH_POWER_MANAGER_POWER_IS_ON_SIGNAL);
 }
 
 RFHandle InitialState(LegControlModule_t* const me, RFEvent *const evt)
 {
 	(void)evt;
 	(void)me;
+	// initialise ports for motor drivers
 
-	// TODO: Initialise motor driver for preTensioning motor in
-	// the following way:
-	//CP_HD_LED_initialiseLED(CP_HD_CPUActivityLED);
-
-	INITIAL_TRANSITION((RFAgent*)me, &NoControlState);
+	INITIAL_TRANSITION((RFAgent*)me, &WaitForPower);
 }
 
-RFHandle NoControlState(LegControlModule_t* const me, RFEvent *const evt)
+RFHandle WaitForPower(LegControlModule_t* const me, RFEvent *const evt)
+{
+	switch (evt->signalValue)
+	{
+	case RF_INITIAL_SIGNAL:
+	case RF_ENTRY_SIGNAL:
+	case RF_EXIT_SIGNAL:
+	{
+		return RF_HANDLED;
+	}
+	case SS_CP_BH_POWER_MANAGER_POWER_IS_ON_SIGNAL:
+	{
+		// enable motor driver for pre-tensioner
+		EXECUTE_TRANSITION((RFAgent const*)me, &NoMotionState);
+	}
+	}
+	return RF_UNHANDLED;
+}
+
+RFHandle NoMotionState(LegControlModule_t* const me, RFEvent *const evt)
 {
 	switch (evt->signalValue)
 	{
 		case RF_INITIAL_SIGNAL:
 		{
-			RFTimer_armTimer(&me->LegControlWaitForPowerUpTimeout, LEG_CONTROL_WAIT_FOR_POWER_UP_TIMEOUT_IN_MS);
-			return RF_HANDLED;
-		}
-		case LEG_CONTROL_WAIT_FOR_POWER_UP_SIGNAL:
-		{
-			// return RF_HANDLED inside
-			//EXECUTE_TRANSITION((RFAgent*)me, &LEDOffState);
-			RFTimer_disarmTimer(&me->LegControlWaitForPowerUpTimeout);
 			return RF_HANDLED;
 		}
 		case RF_ENTRY_SIGNAL:
@@ -67,7 +66,6 @@ RFHandle NoControlState(LegControlModule_t* const me, RFEvent *const evt)
 		}
 		case RF_EXIT_SIGNAL:
 		{
-			RFTimer_disarmTimer(&me->LegControlWaitForPowerUpTimeout);
 			return RF_HANDLED;
 		}
 	}
