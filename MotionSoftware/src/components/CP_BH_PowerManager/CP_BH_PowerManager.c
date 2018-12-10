9be3c8500ad66a3b521523cf65b59d5ba3006207
woookey <woo.barczyk@gmail.com>
2018-12-10 22:21:14 +0000

    Wait for 1 sec before the power up

--- MotionSoftware/src/components/CP_BH_PowerManager/CP_BH_PowerManager.c
+++ MotionSoftware/src/components/CP_BH_PowerManager/CP_BH_PowerManager.c
@@ -1,6 +1,8 @@
 #include <CP_BH_PowerManager.h>
 #include <RF_agents.h>
 #include <RF_events.h>
+#include <RF_timers.h>
+#include <systemSignals.h>
 #include <CP_HD_LEDDriver.h>
 #include <assert.h>
 
@@ -8,14 +10,26 @@
 typedef struct PowerManager
 {
 	RFAgent baseAgent;
+	RF_Timer waitForPowerUpTimer;
 } PowerManagerAgent;
 
 
 static PowerManagerAgent PowerManagerInstance;
 RFAgent * const PowerManager = (RFAgent* const)&PowerManagerInstance.baseAgent;
 
+typedef enum
+{
+	WAIT_FOR_POWER_UP_SIGNAL = SS_END_OF_SIGNAL_SPACE,
+} PrivateSignals;
+
+typedef enum
+{
+	WAIT_FOR_POWER_UP_TIMEOUT_IN_MS = 1000U,
+} PrivateDefinitions;
+
 static RFHandle initialState(PowerManagerAgent* const self, RFEvent *const evt);
 static RFHandle powerOnState(PowerManagerAgent* const self, RFEvent *const evt);
+static RFHandle waitForPowerUpState(PowerManagerAgent* const self, RFEvent *const evt);
 
 
 void PowerManagerConstructor(RFAgent * const self)
@@ -23,6 +37,7 @@ void PowerManagerConstructor(RFAgent * const self)
 	assert(self == PowerManager);
 	PowerManagerAgent* const me = (PowerManagerAgent* const)self;
 	RFBaseAgentConstructor((RFAgent* const) me, &initialState);
+	RFTimerConstructor((RFAgent* const) me, (RF_Timer*) &me->waitForPowerUpTimer, WAIT_FOR_POWER_UP_SIGNAL);
 }
 
 RFHandle initialState(PowerManagerAgent* const self, RFEvent *const evt)
@@ -32,16 +47,41 @@ RFHandle initialState(PowerManagerAgent* const self, RFEvent *const evt)
 
 	CP_HD_LED_initialiseLED(CP_HD_powerOnIndicationLED);
 	CP_HD_LED_initialiseLED(CP_HD_safetyStopIndicationLED);
-	INITIAL_TRANSITION((RFAgent*) self, &powerOnState);
+	CP_HD_LED_switchLEDOff(CP_HD_powerOnIndicationLED);
+	CP_HD_LED_switchLEDOff(CP_HD_safetyStopIndicationLED);
+	INITIAL_TRANSITION((RFAgent*) self, &waitForPowerUpState);
 }
 
-RFHandle powerOnState(PowerManagerAgent* const self, RFEvent *const evt)
+RFHandle waitForPowerUpState(PowerManagerAgent* const self, RFEvent *const evt)
 {
 	switch(evt->signalValue)
 	{
 	case RF_INITIAL_SIGNAL:
 	{
 		CP_HD_LED_switchLEDOn(CP_HD_safetyStopIndicationLED);
+		RFTimer_armTimer((RF_Timer*) &self->waitForPowerUpTimer, WAIT_FOR_POWER_UP_TIMEOUT_IN_MS);
+		return RF_HANDLED;
+	}
+	case WAIT_FOR_POWER_UP_SIGNAL:
+	{
+		RFTimer_disarmTimer((RF_Timer*) &self->waitForPowerUpTimer);
+		EXECUTE_TRANSITION((RFAgent const*)&self->baseAgent, &powerOnState);
+	}
+	case RF_EXIT_SIGNAL:
+	{
+		CP_HD_LED_switchLEDOff(CP_HD_safetyStopIndicationLED);
+		return RF_HANDLED;
+	}
+	}
+	return RF_UNHANDLED;
+}
+
+RFHandle powerOnState(PowerManagerAgent* const self, RFEvent *const evt)
+{
+	switch(evt->signalValue)
+	{
+	case RF_ENTRY_SIGNAL:
+	{
 		CP_HD_LED_switchLEDOn(CP_HD_powerOnIndicationLED);
 		return RF_HANDLED;
 	}
