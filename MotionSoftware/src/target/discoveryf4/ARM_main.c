2b35a4cbbbd5c61dd6fba061eca26442a07f8b44
woookey <woo.barczyk@gmail.com>
2018-12-11 23:19:17 +0000

    Register number of modules for dispatcher before constructing them in the main function

--- MotionSoftware/src/target/discoveryf4/ARM_main.c
+++ MotionSoftware/src/target/discoveryf4/ARM_main.c
@@ -1,5 +1,6 @@
 #include <LEDManager.h>
 #include <CP_BH_PowerManager.h>
+#include <CP_BH_LegControl.h>
 #include <CP_HA_HardwareSetup.h>
 #include <RF_events.h>
 #include <RF_scheduler.h>
@@ -13,16 +14,19 @@ int main()
 	setupHardware();
 	RFEvent LEDManagerPool[10];
 	RFEvent PowerManagerPool[10];
+	RFEvent LegControlModulePool[10];
 	RF_DispatcherCtor();
-
+	RF_Dispatcher_RegisterNumberOfAgents(3);
+	RF_Dispatcher_RegisterNumberOfEvents(SYSTEM_SIGNAL_NUMBER_OF_SIGNALS);
 
 	startAgent(LEDManager, &LEDManagerConstructor, AGENT_PRIORITY_0,
 						&LEDManagerPool[0], sizeof(RFEvent)*10);
 	startAgent(PowerManager, &PowerManagerConstructor, AGENT_PRIORITY_0,
 						&PowerManagerPool[0], sizeof(RFEvent)*10);
+	startAgent(LegControlModule, &CP_BH_LegControlModuleConstructor, AGENT_PRIORITY_0,
+							&LegControlModulePool[0], sizeof(RFEvent)*10);
 
-	RF_Dispatcher_RegisterNumberOfAgents(2);
-	RF_Dispatcher_RegisterNumberOfEvents(SYSTEM_SIGNAL_NUMBER_OF_SIGNALS);
+
 	startHardware();
 	/**
 	 * TODO: Investigate if there should be a lock for starting the framework
