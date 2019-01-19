73899c5056cf1eed6aad6ce4566e31093d874fcb
woookey <woo.barczyk@gmail.com>
2019-01-19 10:55:25 +0000

    Add GPIOs to enable motor and choose direction of rotation

---
+++ MotionSoftware/src/components/CP_HA_GPIO/synthetic/CP_HA_GPIO.c
@@ -0,0 +1,49 @@
+#include <CP_HA_GPIO.h>
+#include <stdio.h>
+#include <string.h>
+#include "GPIODefinition.h"
+
+#define PRETENSION_MOTOR_CLOCKWISE_DIRECTION_GPIO_PORT 0
+#define PRETENSION_MOTOR_ANTICLOCKWISE_DIRECTION_GPIO_PORT 1
+#define PRETENSION_MOTOR_ENABLE_GPIO_PORT 2
+
+
+static struct GPIOStruct pretensionMotorClockwiseDirectionInstance =
+{
+		.pin = (uint8_t)PRETENSION_MOTOR_CLOCKWISE_DIRECTION_GPIO_PORT,
+		.port = 'D',
+};
+
+struct GPIOStruct pretensionMotorAnticlockwiseDirectionInstance =
+{
+		.pin = (uint8_t)PRETENSION_MOTOR_ANTICLOCKWISE_DIRECTION_GPIO_PORT,
+		.port = 'D',
+};
+
+struct GPIOStruct pretensionMotorEnableInstance =
+{
+		.pin = (uint8_t)PRETENSION_MOTOR_ENABLE_GPIO_PORT,
+		.port = 'D',
+
+};
+
+CP_HA_GPIO pretensionMotorClockwiseDirection = &pretensionMotorClockwiseDirectionInstance;
+CP_HA_GPIO pretensionMotorAnticlockwiseDirection = &pretensionMotorAnticlockwiseDirectionInstance;
+CP_HA_GPIO pretensionMotorEnable = &pretensionMotorEnableInstance;
+
+
+
+void CP_HA_initialiseGPIO(CP_HA_GPIO GPIOInstance)
+{
+	printf("GPIO %c%d constructed\n", GPIOInstance->port, GPIOInstance->pin);
+}
+
+void CP_HA_turnGPIOOn(CP_HA_GPIO GPIOInstance)
+{
+	printf("GPIO %c%d switched on\n", GPIOInstance->port, GPIOInstance->pin);
+}
+
+void CP_HA_turnGPIOOff(CP_HA_GPIO GPIOInstance)
+{
+	printf("GPIO %c%d switched off\n", GPIOInstance->port, GPIOInstance->pin);
+}
---
+++ MotionSoftware/src/components/CP_HA_GPIO/synthetic/GPIODefinition.h
@@ -0,0 +1,12 @@
+#ifndef GPIO_DEFINITION_SYNTHETIC_H
+#define GPIO_DEFINITION_SYNTHETIC_H
+
+#include <stdint.h>
+
+struct GPIOStruct
+{
+	uint8_t pin;
+	char port;
+};
+
+#endif
