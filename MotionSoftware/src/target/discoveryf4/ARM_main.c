cddd8b1ce2573bafe6b6af3cfb9d4e299fc6be8c
woookey <woo.barczyk@gmail.com>
2019-02-05 23:16:32 +0000

    construct loggingManager

--- MotionSoftware/src/target/discoveryf4/ARM_main.c
+++ MotionSoftware/src/target/discoveryf4/ARM_main.c
@@ -1,6 +1,7 @@
 #include <LEDManager.h>
 #include <CP_BH_PowerManager.h>
 #include <CP_BH_LegControl.h>
+#include <CP_BH_LoggingManager.h>
 #include <CP_HA_HardwareSetup.h>
 #include <RF_events.h>
 #include <RF_scheduler.h>
@@ -15,8 +16,10 @@ int main()
 	RFEvent LEDManagerPool[10];
 	RFEvent PowerManagerPool[10];
 	RFEvent LegControlModulePool[10];
+	RFEvent LoggingManagerPool[10];
+
 	RF_DispatcherCtor();
-	RF_Dispatcher_RegisterNumberOfAgents(3);
+	RF_Dispatcher_RegisterNumberOfAgents(4);
 	RF_Dispatcher_RegisterNumberOfEvents(SYSTEM_SIGNAL_NUMBER_OF_SIGNALS);
 
 	startAgent(LEDManager, &LEDManagerConstructor, AGENT_PRIORITY_0,
@@ -25,6 +28,8 @@ int main()
 						&PowerManagerPool[0], sizeof(RFEvent)*10);
 	startAgent(LegControlModule, &CP_BH_LegControlModuleConstructor, AGENT_PRIORITY_0,
 							&LegControlModulePool[0], sizeof(RFEvent)*10);
+	startAgent(LoggingManager, &CP_BH_LoggingManagerConstructor, AGENT_PRIORITY_0,
+								&LoggingManagerPool[0], sizeof(RFEvent)*10);
 
 
 	startHardware();
