776ffe31576640d3b19231865b0f008739d66af0
woookey <woo.barczyk@gmail.com>
2019-02-05 23:25:06 +0000

    Create CP_HD_Logger

---
+++ MotionSoftware/src/components/CP_HD_Logger/CP_HD_Logger.c
@@ -0,0 +1,17 @@
+#include <CP_HD_Logger.h>
+
+/**
+ * Initialises the logger
+ */
+void CP_HD_Logger_initialise(void)
+{
+
+}
+
+/**
+ * Send any message packet
+ */
+void CP_HD_Logger_sendData(void)
+{
+
+}
---
+++ MotionSoftware/src/components/CP_HD_Logger/CP_HD_Logger.h
@@ -0,0 +1,14 @@
+#ifndef CP_HD_LOGGER_H
+#define CP_HD_LOGGER_H
+
+/**
+ * Initialises the logger
+ */
+void CP_HD_Logger_initialise(void);
+
+/**
+ * Send any message packet
+ */
+void CP_HD_Logger_sendData(void);
+
+#endif
